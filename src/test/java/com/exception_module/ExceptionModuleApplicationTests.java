5554ab4364f9c485c037fa0ee94771b843500e07
Gupuroom <cjsrnr1114@naver.com>
2025-01-31 15:22:54 +0900

    init

---
+++ src/test/java/com/exception_module/ExceptionModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exception_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
